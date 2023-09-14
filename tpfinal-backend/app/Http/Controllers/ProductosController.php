be707f81d7075b4526c8eb0a2d7c98c40553d723
Martina Coassin <76189888+MarCoass@users.noreply.github.com>
2023-09-14 16:12:32 -0300

    agregado el update y show

--- tpfinal-backend/app/Http/Controllers/ProductosController.php
+++ tpfinal-backend/app/Http/Controllers/ProductosController.php
@@ -26,7 +26,7 @@ class ProductosController extends Controller
         $producto->stock = $request->input('stock');
         $producto->estado = $request->input('estado');
 
-        
+
         if ($request->hasFile('imagen')) {
             $extension = $request->file('imagen')->getClientOriginalExtension(); //obtengo el tipo de extensión
             $nombreSinEspacios = str_replace(' ', '', $request->input('nombre')); //saco los espacios para evitar problemas
@@ -44,12 +44,47 @@ class ProductosController extends Controller
     }
 
 
-    public function update(Request $request)
+    public function update(Request $request, $id)
     {
+        // Busca el producto por su ID
+        $producto = producto::find($id);
+
+        if (!$producto) {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+
+        // Actualiza los campos del producto con los datos del request
+        $producto->nombre = $request->input('nombre');
+        $producto->descripcion = $request->input('descripcion');
+        $producto->precio = $request->input('precio');
+        $producto->stock = $request->input('stock');
+        $producto->estado = $request->input('estado');
+
+        // Actualiza la imagen si se proporciona en el request
+        if ($request->hasFile('imagen')) {
+            $extension = $request->file('imagen')->getClientOriginalExtension();
+            $nombreSinEspacios = str_replace(' ', '', $request->input('nombre'));
+            $nombreImagen = $nombreSinEspacios . '.' . $extension;
+            $path = $request->file('imagen')->storeAs('/productos', $nombreImagen, 'public');
+            $producto->url_imagen = $path;
+        }
+
+        // Actualiza la ciudad asociada al producto si se proporciona en el request
+        if ($request->has('ciudad')) {
+            $ciudad = Ciudad::find($request->input('ciudad'));
+            $producto->Ciudad()->associate($ciudad);
+        }
+
+        $producto->save();
+
+        return response()->json(['message' => 'Producto actualizado exitosamente'], 200);
     }
 
-    public function show($idProducto)
+
+    public function show($id)
     {
+        $producto = producto::find($id);
+        return response()->json($producto);
     }
 
     public function delete($id)
