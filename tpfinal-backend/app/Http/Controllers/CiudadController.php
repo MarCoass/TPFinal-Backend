983a392b0ce4e2d5f4c04418155c5cdbb838694d
Martina Coassin <martycoassin@gmail.com>
2023-09-24 20:07:58 -0300

    arreglando cagadas

--- tpfinal-backend/app/Http/Controllers/CiudadController.php
+++ tpfinal-backend/app/Http/Controllers/CiudadController.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Ciudad;
 use Illuminate\Http\Request;
 
 class CiudadController extends Controller
 {
-    
+    //
     public function index()
     {
         $ciudades = Ciudad::all();
