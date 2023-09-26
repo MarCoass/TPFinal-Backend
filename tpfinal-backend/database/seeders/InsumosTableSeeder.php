dd307a98b0b5f04a4c123b6481892c6c5208a7be
Martina Coassin <martycoassin@gmail.com>
2023-09-25 21:54:20 -0300

    seeder

--- tpfinal-backend/database/seeders/InsumosTableSeeder.php
+++ tpfinal-backend/database/seeders/InsumosTableSeeder.php
@@ -73,5 +73,66 @@ class InsumosTableSeeder extends Seeder
             'stock_minimo' => '1',
             'tags' => 'largo',
         ]);
+        Insumo::create([//7
+            'nombre' => 'Esmalte violeta 99',
+            'descripcion' => 'Esmalte semipermanente',
+            'marca' => 'City Girl',
+            'stock' => '1',
+            'estado' => '1',
+            'id_categoria' => '2',
+            'stock_minimo' => '1',
+            'tags' => 'violeta',
+        ]);
+        Insumo::create([//8
+            'nombre' => 'Esmalte Rojo 40',
+            'descripcion' => 'Esmalte semipermanente',
+            'marca' => 'City Girl',
+            'stock' => '1',
+            'estado' => '1',
+            'id_categoria' => '2',
+            'stock_minimo' => '1',
+            'tags' => 'rojo',
+        ]);
+        Insumo::create([//9
+            'nombre' => 'Esmalte Rojo Oscuro 38',
+            'descripcion' => 'Esmalte semipermanente',
+            'marca' => 'City Girl',
+            'stock' => '1',
+            'estado' => '1',
+            'id_categoria' => '2',
+            'stock_minimo' => '1',
+            'tags' => 'rojo',
+        ]);
+        Insumo::create([//10
+            'nombre' => 'Esmalte Rosita 111',
+            'descripcion' => 'Esmalte semipermanente',
+            'marca' => 'City Girl',
+            'stock' => '1',
+            'estado' => '1',
+            'id_categoria' => '2',
+            'stock_minimo' => '1',
+            'tags' => 'rosita',
+        ]);
+        Insumo::create([//11
+            'nombre' => 'Esmalte Blanco 32',
+            'descripcion' => 'Esmalte semipermanente',
+            'marca' => 'City Girl',
+            'stock' => '1',
+            'estado' => '1',
+            'id_categoria' => '2',
+            'stock_minimo' => '1',
+            'tags' => 'blanco',
+        ]);
+        Insumo::create([//11
+            'nombre' => 'Esmalte Negro 31',
+            'descripcion' => 'Esmalte semipermanente',
+            'marca' => 'City Girl',
+            'stock' => '1',
+            'estado' => '1',
+            'id_categoria' => '2',
+            'stock_minimo' => '1',
+            'tags' => 'blanco',
+        ]);
+        
     }
 }
