9910f439665632779e1d735565f169a9161f50b6
Martina Coassin <martycoassin@gmail.com>
2023-10-19 09:34:34 -0300

    Update proveedores.php

--- tpfinal-backend/routes/proveedores.php
+++ tpfinal-backend/routes/proveedores.php
@@ -1,16 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProveedorController;
-use App\Models\proveedor;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['rol:1']], function () {
 
-    Route::get(['/proveedores', [ProveedorController::class, 'index']]);
-    Route::get(['/proveedor/{id}', [ProveedorController::class, 'show']]);
-    Route::post(['/proveedorStore', [ProveedorController::class, 'store']]);
-    Route::post(['/proveedorUpdate/{id}', [ProveedorController::class, 'update']]);
-    Route::post(['/proveedorDelete/{id}', [ProveedorController::class, 'delete']]);
+    Route::get('/proveedores', [ProveedorController::class, 'index']);
+    Route::get('/proveedor/{id}', [ProveedorController::class, 'show']);
+    Route::post('/proveedorStore', [ProveedorController::class, 'store']);
+    Route::post('/proveedorUpdate/{id}', [ProveedorController::class, 'update']);
+    Route::post('/proveedorDelete/{id}', [ProveedorController::class, 'delete']);
 });
 
 
