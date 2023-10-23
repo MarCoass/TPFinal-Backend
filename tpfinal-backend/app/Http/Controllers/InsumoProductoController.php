31246244f577205471757c4ec129038ce3d72ab6
Martina Coassin <martycoassin@gmail.com>
2023-10-23 17:13:49 -0300

    funciona modificar el stock

--- tpfinal-backend/app/Http/Controllers/InsumoProductoController.php
+++ tpfinal-backend/app/Http/Controllers/InsumoProductoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\insumo;
 use App\Models\insumoProducto;
 use Illuminate\Http\Request;
 
@@ -16,17 +17,31 @@ class InsumoProductoController extends Controller
         $insumoProducto->save();
     }
 
-    public function buscarPorProducto($id){
+    public function buscarPorProducto($id)
+    {
         $insumoProductos = insumoProducto::where('id_producto', $id)->get();
         return response()->json($insumoProductos);
     }
 
-    public function actualizarCantidad(Request $request){
+    public function actualizarCantidad(Request $request)
+    {
         $id_insumo = $request->input('id_insumo');
-        $id_prodducto = $request->input('id_producto');
-        $insumoProducto = insumoProducto::where('id_insumo', $id_insumo)->where('id_producto', $id_prodducto)->first();
+        $id_producto = $request->input('id_producto');
+        $insumoProducto = insumoProducto::where('id_insumo', $id_insumo)->where('id_producto', $id_producto)->first();
         $insumoProducto->cantidad = $request->input('cantidad');
         $insumoProducto->save();
         return response()->json(['message' => 'Producto actualizado exitosamente'], 200);
+    }
+
+    public function modificarStockPorProductos($cantidadProducto, $id_producto)
+    {
+        $insumoProductos = insumoProducto::where('id_producto', $id_producto)->get();
+        foreach ($insumoProductos as $insumoProducto) {
+            $cantidadNecesaria = $insumoProducto->cantidad * $cantidadProducto;
+            $insumo = insumo::find($insumoProducto->id_insumo);
+            $insumo->stock = $insumo->stock - $cantidadNecesaria;
+            $insumo->save();
+        }
+        return response()->json('Exito descontando stock');
     }
 }
