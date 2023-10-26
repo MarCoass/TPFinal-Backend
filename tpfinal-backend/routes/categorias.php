27c78fcc55dea2c1af315eab25f2e45c467f8c6f
Martina Coassin <76189888+MarCoass@users.noreply.github.com>
2023-10-26 15:31:40 -0300

    Update categorias.php

--- tpfinal-backend/routes/categorias.php
+++ tpfinal-backend/routes/categorias.php
@@ -10,7 +10,7 @@ Route::get('/administracion/categoriasInsumos', [CategoriaInsumos::class, 'index
 Route::post('/administracion/categoriasInsumosStore', [CategoriaInsumos::class, 'store']);
 Route::delete('/administracion/categoriasInsumosDelete/{id}', [CategoriaInsumos::class, 'delete']);
 Route::post('/administracion/categoriasInsumosUpdate/{id}', [CategoriaInsumos::class, 'update']);
-Route::get('//administracion/categoriaInsumo/{id}', [CategoriaInsumos::class, 'show']);
+Route::get('/administracion/categoriaInsumo/{id}', [CategoriaInsumos::class, 'show']);
 
 //------ CATEGORIAS DE PRODUCTOS -------//
 Route::get('/categoriasSets', [CategoriaSetController::class, 'index']);
