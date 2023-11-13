a7d8b47205f1037f1f297ead5c4bcd843d8cdb12
Martina Coassin <martycoassin@gmail.com>
2023-11-13 16:42:39 -0300

    pedidos personalizados

--- tpfinal-backend/app/Http/Controllers/PedidosController.php
+++ tpfinal-backend/app/Http/Controllers/PedidosController.php
@@ -24,6 +24,22 @@ class PedidosController extends Controller
     }
 
     //store
+    public function store(Request $request)
+    {
+
+        $pedido = new pedidoPersonalizado();
+        $set = new SetController();
+        $setResponse = $set->store($request);
+
+        //obtengo el ID desde el response
+        $data = json_decode($setResponse->getContent());
+        $id_producto = $data->id_producto;
+        $pedido->id_producto = $id_producto;
+        $pedido->id_usuario = $request->input('id_user');
+
+        $pedido->save();
+        return response()->json(['message' => 'Datos guardados exitosamente'], 200);
+    }
 
     //update
 
