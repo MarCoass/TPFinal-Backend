97e46efcb3186a4880c94c190cf3c0cd0e016269
Martina Coassin <martycoassin@gmail.com>
2023-11-16 15:13:59 -0300

    funciona comentar cliente

--- tpfinal-backend/app/Http/Controllers/ClienteController.php
+++ tpfinal-backend/app/Http/Controllers/ClienteController.php
@@ -8,12 +8,19 @@ use Illuminate\Http\Request;
 class ClienteController extends Controller
 {
     public function index(){
-        $clientes = User::all();
+        $clientes = User::where('id_rol', 2)->get();
         return response()->json($clientes);
     }
 
     public function show($id){
         $cliente = User::find($id);
         return response()->json($cliente);
+    }
+
+    public function comentar(Request $request, $id){
+        $cliente = User::find($id);
+        $cliente->observacion = $request->input('observacion');
+        $cliente->save();
+        return response()->json(['exito' => true, 'message' => 'Comentario modificado exitosamente', 200]);
     }
 }
--- tpfinal-backend/routes/cliente.php
+++ tpfinal-backend/routes/cliente.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/administracion/clientes', [ClienteController::class, 'index']);
 Route::get('/administracion/cliente/{id}', [ClienteController::class, 'show']);
+Route::post('/administracion/clienteComentar/{id}', [ClienteController::class, 'comentar']);
